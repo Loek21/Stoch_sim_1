07dc1f302ab1a7e7a7cefd15c9b50b8402a6498c
Sebastiaan Kruize <sebaskruize@gmail.com>
2020-11-16 17:46:04 +0100

    added results stratified etc

--- randomhalved.py
+++ randomhalved.py
@@ -9,9 +9,9 @@ import sys
 ## -0.75 tot 2nd blob
 ## 0.47 tot last blob
 # max_iterations = [100,500,1000,1500]
-max_iterations = 1500
-width = 600
-height = 400
+max_iterations = [100, 500, 1000, 2500]
+N_list = [100, 1000, 10000, 100000]
+alg_iterations = 100
 
 def mandelbrot(c, max):
   z = 0
@@ -23,43 +23,46 @@ def mandelbrot(c, max):
 
   return n
 
-def MonteCarlo(N): 
+def MonteCarlo(N, recursions): 
     correct = 0
 
     for _ in range(N):
-        if mandelbrot(np.random.uniform(-1.79, 0.47) + np.random.uniform(0, 1)*1j, max_iterations) == max_iterations:
+        if mandelbrot(np.random.uniform(-1.79, 0.47) + np.random.uniform(0, 1)*1j, recursions) == recursions:
             correct += 1
 
     surface = (0.47 + 1.79) * correct / N
 
     return surface * 2
 
-areas = []
-area_stds = []
-N_list = [200,400,800, 1600, 5000]
-alg_iterations = 100
-for i in N_list:
-    area_list = []
-    for _ in range(alg_iterations):
-        area = MonteCarlo(i)
-        area_list.append(area)
+for recursions in max_iterations:
+    areas = []
+    area_stds = []
+    for i in N_list:
+        area_list = []
+        for _ in range(alg_iterations):
+            area = MonteCarlo(i, recursions)
+            area_list.append(area)
 
-    areas.append(np.mean(np.array(area_list)))
-    area_stds.append(np.std(np.array(area_list)))
+        areas.append(np.mean(np.array(area_list)))
+        area_stds.append(np.std(np.array(area_list)))
 
-areas = np.array(areas)
-area_stds = np.array(area_stds)
+    areas = np.array(areas)
+    area_stds = np.array(area_stds)
 
-plt.plot(N_list, areas, label="Strat sampling")
-plt.fill_between(N_list, areas+area_stds, areas-area_stds, alpha=0.2)
-plt.plot(N_list, area_stds*1.96/np.sqrt(alg_iterations))
+    #plt.plot(N_list, areas, label="Strat sampling")
+    #plt.fill_between(N_list, areas+area_stds, areas-area_stds, alpha=0.2)
+    #plt.plot(N_list, area_stds*1.96/np.sqrt(alg_iterations))
 
-conf_radii = area_stds*1.96/np.sqrt(alg_iterations)
+    conf_radii = area_stds*1.96/np.sqrt(alg_iterations)
 
-for mean, N, conf in zip(areas, N_list, conf_radii):
-    print(f"Runs {N}, Mean {mean}, Confidence radius {conf}")
+    file_aux = open(f'results_halved_{recursions}.csv','a')
+    file_aux.write("N_points,Mean_surface,Std_surface,confidence_radius")
 
+    for mean, N, conf, std in zip(areas, N_list, conf_radii, area_stds):
+        print(f"Sample per run {N}, Mean {mean}, Confidence radius {conf}, Max recursions {recursions}")
+        
+        file_aux.write("\n"+str(N)+","+str(mean)+","+str(std)+ ","+str(conf))
 
+    file_aux.close()
 
-
-plt.show()
+    #plt.show()
